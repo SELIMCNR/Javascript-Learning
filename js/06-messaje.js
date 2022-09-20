53b1a681d9f83f08ebfca3d070527967b7b76136
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2022-09-20 20:27:55 +0300

    Javascript
    Javascript

---
+++ js/06-messaje.js
@@ -0,0 +1,11 @@
+function Call() {
+    alert("Benin hangi fonksiyon çağırdı?");
+    document.getElementById('result').innerHTML = "Javascript ne kadar canlı";
+    document.getElementById('result').innerHTML = "Javascript kolay bir dilmiş";
+}
+function welcome() {
+    alert("Hoşgeldiniz");
+}
+function goodbye() {
+    alert("Güle güle")
+}
